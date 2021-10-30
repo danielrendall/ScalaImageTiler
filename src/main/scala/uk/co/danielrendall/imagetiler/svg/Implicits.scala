e800dfd62064b3d4f3f84f77b711ca42ec41e085
Daniel Rendall <drendall@gmail.com>
2021-10-30 13:19:35 +0100

    Realised we probably don't need an implicit TileContext

--- src/main/scala/uk/co/danielrendall/imagetiler/svg/Implicits.scala
+++ src/main/scala/uk/co/danielrendall/imagetiler/svg/Implicits.scala
@@ -2,14 +2,13 @@ package uk.co.danielrendall.imagetiler.svg
 
 import org.w3c.dom.Element
 import uk.co.danielrendall.imagetiler.interfaces.Customiser
-import uk.co.danielrendall.imagetiler.model.TileContext
 import uk.co.danielrendall.mathlib.geom2d.shapes.Shape
 
 object Implicits {
   implicit class SvgElementOps(element: Element) {
 
     def appendShape[T <: Shape[T]](shape: T, customiser: Customiser = identity)
-                                  (implicit svgRenderable: SvgRenderable[T], tileContext: TileContext): Unit = {
+                                  (implicit svgRenderable: SvgRenderable[T]): Unit = {
       element.appendChild(customiser(svgRenderable.getElement(shape, element.getOwnerDocument.createElement)))
     }
 
--- src/main/scala/uk/co/danielrendall/imagetiler/svg/SvgRenderable.scala
+++ src/main/scala/uk/co/danielrendall/imagetiler/svg/SvgRenderable.scala
@@ -1,18 +1,17 @@
 package uk.co.danielrendall.imagetiler.svg
 
 import org.w3c.dom.Element
-import uk.co.danielrendall.imagetiler.model.TileContext
 import uk.co.danielrendall.mathlib.geom2d.shapes.{Polygon, Shape}
 
 trait SvgRenderable[T <: Shape[T]] {
-  def getElement(shape: T, elementFactory: String => Element)(implicit tileContext: TileContext): Element
+  def getElement(shape: T, elementFactory: String => Element): Element
 }
 
 object SvgRenderable {
 
   implicit object PolygonSvgRenderable extends SvgRenderable[Polygon] {
     override def getElement(shape: Polygon,
-                            elementFactory: String => Element)(implicit tileContext: TileContext): Element = {
+                            elementFactory: String => Element): Element = {
       val pathEl = elementFactory("path")
       val lineString = shape.points.map(p => s"${p.x} ${p.y} ").mkString("M ", "L ", "z")
       pathEl.setAttributeNS(null, "d", lineString)
